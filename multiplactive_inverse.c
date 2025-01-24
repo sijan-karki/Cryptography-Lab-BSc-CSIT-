372e64a3337c948bb8accea2dfd783fc5ab9c14b
SijanUnleashed <112467487+sijan-karki@users.noreply.github.com>
2025-01-24 18:22:57 +0545

    Add files via upload

---
+++ multiplactive_inverse.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+main() {
+  int i, num, MI;
+  int executenext = 1;
+  do {
+    printf("\nEnter number to get multiplicative Inverse: ");
+    scanf("%d", & num);
+    for (i = 1; i <= num; i++) {
+      MI = ((i * 26) + 1);
+      if (MI % num == 0) {
+        break;
+      }
+    }
+    MI = MI / num;
+    printf("Multiplicative inverse of %d is %d\n", num, MI);
+    printf("Do you want to execute next? (1 for Yes, 0 for No): ");
+    scanf("%d", & executenext);
+    getchar();
+  } while (executenext);
+  printf("------------Sijan Karki-------------");
+  printf("\n--------- Rollno:28 --------------");
+
+}
